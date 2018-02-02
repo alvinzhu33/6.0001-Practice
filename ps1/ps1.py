ff33b3769b45004f5a277fcf16a49b5f6cd5578e
AZ <alvinzhu33@yahoo.com>
2018-02-01 22:40:53 -0500

    Fix

--- ps1/ps1.py
+++ ps1/ps1.py
@@ -47,59 +47,42 @@ portion_down_payment = .25;
 r = 0.04;
 semi_annual_raise = .07;
 
-portion_saved = .5;
+maxRate = 1;
+minRate = 0;
 search_steps = 0;
 target = False;
+minTarget = (total_cost*.25)-100;
+maxTarget = (total_cost*.25)+100;
 
-while not target:
+
+while not target and search_steps < 20:
+    portion_saved = (maxRate+minRate)/2;
+    portion_saved = round(portion_saved*10000)/10000;
     current_savings = 0;
     annual_salary = salary;
     monthly_salary = annual_salary/12;
     search_steps += 1;
     months = 0;
-    #print(portion_saved, search_steps);
-    while current_savings < (total_cost*.25)-100 and months <= 40:
+    while months < 36:
         current_savings += current_savings*r/12;
         current_savings += monthly_salary*portion_saved;
         months += 1;
         if months%6 == 0:
             annual_salary += annual_salary*semi_annual_raise;
             monthly_salary = annual_salary/12;
-    #print(current_savings < (total_cost*.25)-100, months);
-    if portion_saved > 2:
-        print("Not possible");
-        break;
-    if months > 36:
-        portion_saved += portion_saved/2;
+    if current_savings < minTarget:
+        if portion_saved>=1:
+            print("Not possible");
+            break;
+        minRate = portion_saved;
         #print("too little")
-    elif months < 36:
-        portion_saved -= portion_saved/2;
+    elif current_savings > maxTarget:
+        maxRate = portion_saved;
         #print("too much")
     else:
         target = True;
         print("Rate:", portion_saved);
         print("Steps:", search_steps)
-    portion_saved = int(round(portion_saved*10000))/10000;
-
-#salary = float(input("Enter annual salary: "));
-#annual_salary = salary;
-#monthly_salary = annual_salary/12;
-#total_cost = 1000000;
-#portion_down_payment = .25;
-#r = 0.04;
-#semi_annual_raise = .07;
-#
-#portion_saved = float(input("Saved"));
-#search_steps = 0;
-#target = False;
-#current_savings = 0;
-#months = 0;
-#
-#while current_savings < (total_cost*.25)-100:
-#        current_savings += current_savings*r/12;
-#        current_savings += monthly_salary*portion_saved;
-#        months += 1;
-#        if months%6 == 0:
-#            annual_salary += annual_salary*semi_annual_raise;
-#            monthly_salary = annual_salary/12;
-#print(current_savings < (total_cost*.25)-100, months);
+#        
+#.4257
+#.2056
